c017a687dd515ecd62a74dffa5c306e93d2b10a6
srleoalex <leoag96@gmail.com>
2022-07-13 06:28:46 -0400

    changed packages.json to api.js for testing

--- app/routes/api.js
+++ app/routes/api.js
@@ -1,33 +1,26 @@
-const router = require("express").Router();
-
-router.get("/getFriends", (req, res) => {
-    res.send("NO FRIENDS");
-});
-
-module.exports = router;
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-const path = require('path');
-const PORT = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 const app = express();
-
-app.set('port', (process.env.PORT || 5000));
 
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get('/viewProfile', async (req, res, next) => 
+app.get("/getFriends", (req, res) => {
+    res.json({message : 'NO FRIENDS'});
+});
+
+app.post('/editProfile', async (req, res) => 
 {
-  // incoming: username, tag
-  // outgoing: username, tag, games, school, error
+  // incoming: discordID, name, gender, school
+  // outgoing: discordID, error
 
   let error = '';
 
-  const { username, tag } = req.body;
+  const { username } = req.body;
 
   let _search = search.trim();
   
@@ -49,4 +42,40 @@ app.get('/viewProfile', async (req, res, next) =>
 
   let ret = { username:usr, tag:tg, games:gm, school:sch, error:''};
   res.status(200).json(ret);
+});
+
+app.get('/viewProfile', async (req, res) => 
+{
+  // incoming: username
+  // outgoing: username, games, school, error
+
+  let error = '';
+
+  const { username } = req.body;
+
+  let _search = search.trim();
+  
+  const db = client.db("<database-name>");
+  const results = await db.collection('Users').find({"Users":{$regex:_search+'.*', $options:'r'}}).toArray();
+  
+  let usr = '';
+  let tg = -1;
+  let gm = [];
+  let sch = -1;
+
+  if( results.length > 0 )
+  {
+    usr = results[0].username;
+    tg = results[0].tag;
+    gm = results[0].games;
+    sch = results[0].school;
+  }
+
+  let ret = { username:usr, tag:tg, games:gm, school:sch, error:''};
+  res.status(200).json(ret);
+});
+
+app.listen(port, () =>
+{
+  console.log('Server listening on port ' + port);
 });
