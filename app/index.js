9cdd2ebbea0aee44de1369946022ee56672eef89
Dagomara <53799719+Dagomara@users.noreply.github.com>
2022-07-21 16:37:11 -0400

    Update index.js
    quick hosting resolution bc I got the filepath wrong!

--- app/index.js
+++ app/index.js
@@ -51,7 +51,7 @@ app.use("/auth", require("./routes/auth"));
 // All other GET requests not handled before will return our React app
 //if (process.env.NODE_ENV === 'production') {
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+    res.sendFile(path.resolve('client', 'build', 'index.html'));
   });
 //}
 
