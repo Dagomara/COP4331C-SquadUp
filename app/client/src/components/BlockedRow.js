7e2fb79530087127fb23dbb64432358fae1916c8
Elias Cousino <106120050+Elias-Cousino@users.noreply.github.com>
2022-07-22 02:38:49 -0400

    cleaned up blocked row with the correct button code and finish bsstudio designs

--- app/client/src/components/BlockedRow.js
+++ app/client/src/components/BlockedRow.js
@@ -4,7 +4,7 @@ export default function BlockedRow(props) {
     return(
         <div class="row friendRow">
             <div class="col-auto align-self-center"><img class="img-fluid rounded-circle friendIcon" src={props.blocked.avatar} /><span>{props.blocked.name}<br /></span></div>
-            <div class="col-auto align-self-center btn btn-primary fw-bold unblockButton bg-gradient-danger"><span class>Unblock User</span></div>
+            <div class="col-auto align-self-center"><button class="btn btn-primary fw-bold bg-gradient-danger" type="button"><span>Unblock Player</span></button></div>
         </div>
     );
 };
