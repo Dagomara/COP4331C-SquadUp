5cbe28edbdef3af76fcd55b6e53fceaff80d468c
Elias Cousino <106120050+Elias-Cousino@users.noreply.github.com>
2022-07-24 00:01:50 -0400

    Update FAQ.js

--- app/client/src/pages/FAQ.js
+++ app/client/src/pages/FAQ.js
@@ -14,7 +14,7 @@ function FAQ() {
               <p style={{color: 'rgb(0,0,0)', fontSize: '18px'}}>&nbsp; &nbsp;1.&nbsp; How do we connect players?<br />We connect players based off of similar rank/level with varying roles in game. We use a discord bot to match each player together onto the same discord server.</p>
               <p style={{color: 'rgb(0,0,0)', fontSize: '18px'}}>&nbsp; &nbsp;2. Do I need an account?<br />No! SquadUp uses Discord as a sign in. All that is needed is a Discord account.</p>
               <p style={{color: 'rgb(0,0,0)', fontSize: '18px'}}>&nbsp; &nbsp;3. What if I find someone toxic?<br />If you ever encounter anyone toxic when queueing you are able to block that specific player.</p>
-              <p style={{color: 'rgb(0,0,0)', fontSize: '18px'}}>&nbsp; &nbsp;4. What if I want to play with the same person at a later date?<br />You are able to add other users as friends and be able to queue with them in the future.</p>
+              {false && (<p style={{color: 'rgb(0,0,0)', fontSize: '18px'}}>&nbsp; &nbsp;4. What if I want to play with the same person at a later date?<br />You are able to add other users as friends and be able to queue with them in the future.</p>)}
             </div>
           </div>
           <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
