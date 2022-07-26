aa074f454c5918320bdfcadd27c291eadc8da7dc
Elias Cousino <106120050+Elias-Cousino@users.noreply.github.com>
2022-07-26 01:29:02 -0400

    added gamemodal component edited profile and changed a color in deletemodal.css

---
+++ app/client/src/components/GameModal.js
@@ -0,0 +1,32 @@
+import React from "react";
+import '../assets/stylesheets/deletemodal.css';
+
+function GameModal({ setGameModal }) {
+    return (
+      <div className="modalBackground">
+        <div className="modalContainer">
+        <div className="modalContainer2">
+          <div className="titleCloseBtn">
+            <button onClick={() => { setGameModal(false);  }}> X </button>
+          </div>
+          <div className="title">
+            <div className="title-border">
+            <h1>Discord Username with Avatar</h1>
+            </div>
+          </div>
+          <div className="body">
+          <div className="body-border">
+            <p>Are you sure you want to delete your account? This action cannot be undone.</p>
+          </div>
+          </div>
+          <div className="footer">
+            <button onClick={() => { setGameModal(false);  }} id="cancelBtn">No</button>
+            <button onClick={() => { setGameModal(false);  }}>Yes</button>
+          </div>
+        </div>
+        </div>
+      </div>
+    );
+  }
+  
+  export default GameModal;
--- app/client/src/pages/Settings.js
+++ app/client/src/pages/Settings.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import axios from "axios";
 import Navbar from '../components/Navbar';
 import { Form, Field } from "@progress/kendo-react-form";
